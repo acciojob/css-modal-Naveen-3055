d92f2329d429c83d871c88e9387880302b129c20
Naveen Kumar <knaveenk869@gmail.com>
2025-10-25 12:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const button = document.getElementById('openModal')
+const modal = document.querySelector('.modal')
+const close = document.getElementById('close-button');
+
+button.addEventListener('click',()=>{
+	modal.style.display = 'flex'
+	button.style.background = 'gray'
+})
+close.addEventListener('click',()=>{
+	modal.style.display = 'none'
+	button.style.background = 'none'
+})
+window.addEventListener('click',(event)=>{
+	if(event.target === modal){
+modal.style.display='none'
+		button.style.background = 'none'
+	}
+})
